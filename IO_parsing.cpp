9b8fc1a9b2b314265414ee14c96cabd4e557efa6
MahdyZ7 <ayassin@MahdyZ7>
2023-12-15 12:54:03 +0400

    finds and replaces the word only

--- IO_parsing.cpp
+++ IO_parsing.cpp
@@ -1,54 +1,81 @@
-#include "iostream"
-#include "fstream"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#define WORD "__added_filler_instance_"
 
-int main(int argc, char **argv)
-{
+typedef std::map<std::string, std::vector<std::string> > map_vector_t;
+
+std::string getFillName(std::string line);
+void outputLines(std::ofstream &outfile, std::vector<std::string> lines);
+
+int main(int argc, char **argv) {
+
 	// check for correct no of args
 	if (argc != 2)
 	{
 		std::cout << "Enter name of file as argument eg ./ioparsing filename.def \n";
-		return (0);
+		return (1);
 	}
-	std::cout << "Enter the name of the output file\n";
-	// std::string		line;
-	// std::string		outfile;
-	// std::string		sub_out = std::string("ordered") + std::string(argv[1]);
-	// std::string		sub_in = argv[1];
-	// std::ifstream	fin;
-	// std::ofstream 	fout;
-	// char 			*char_arr;
-	// // open the read file
-	// fin.open(argv[1]);
-	// if (fin == 0)
-	// {
-	// 	std::cout << "The file " << argv[1] << " does not exist\n";
-	// 	return (0);
-	// }
-	// // open the write file;
-	// outfile = std::string("ordered_") + std::string(argv[1]);
-	// char_arr = &outfile[0];
-	// fout.open(char_arr);
-	// if (fout == 0)
-	// {
-	// 	fin.close();
-	// 	std::cout << argv[1] << ": Permistion denied\n";
-	// 	return (0);
-	// }
-	// // loop to read and write
-	// while (getline(fin, line))
-	// {
-	// 	for (int pos = 0; sub_out[0] && line.find(sub_out, pos) < line.length();)
-	// 	{
-	// 		pos = line.find(argv[2], pos);
-	// 		line = line.substr(0, pos) + sub_in +
-	// 			line.substr(pos + sub_out.length(), -1);
-	// 		pos = (line.substr(0, pos) + sub_in).length();
-	// 	}
-	// 	fout << line;
-	// 	if (fin.eof() != 1)
-	// 		fout << "\n";
-	// }
-	// fin.close();
-	// fout.close();
-	// return (0);
+    std::ifstream inputFile(argv[1]);
+    std::ofstream outputFile("sorted_output.txt");
+    
+    if (!inputFile.is_open() || !outputFile.is_open()) {
+        std::cerr << "Could not open files!" << std::endl;
+        return 1;
+    }
+    
+    std::string line;
+	map_vector_t linesWithNumbers;
+    int lineNumber = 0;
+    
+    while (getline(inputFile, line)) {
+        std::stringstream ss;
+        ss << lineNumber << " " << line;
+		if (line.find("FILL") != std::string::npos)
+        	(linesWithNumbers[getFillName(line)]).push_back(line);
+			// outputFile << line << '\n';
+        ++lineNumber;
+    }
+    
+    for (const auto &p : linesWithNumbers)
+		outputLines(outputFile, p.second);
+    
+    inputFile.close();
+    outputFile.close();
+	std::remove("original.txt");
+   	std::rename("temp.txt", "original.txt");
+    
+    return 0;
+}
+
+std::string getFillName(std::string line)
+{
+	std::stringstream ss(line);
+	std::string word;
+	while (ss >> word) {		
+		if (word.find("FILL") != std::string::npos)
+			return word;
+	}
+	return "";
+}
+
+void outputLines(std::ofstream &outfile, std::vector<std::string> lines)
+{
+	unsigned int i = 0;
+	for (const auto &line : lines)
+	{
+		std::stringstream ss(line);
+		std::string word;
+		while (ss >> word) {		
+			if (word.find(WORD) != std::string::npos)
+				word = word.substr(0, word.rfind('_') + 1) + std::to_string(i++);
+			outfile << word << " ";
+		}
+		outfile << std::endl;
+	}
 }
---
+++ Makefile
@@ -0,0 +1,25 @@
+NAME = fillparsing
+
+SRC = IO_parsing.cpp
+
+CC = c++
+
+CFLAGS = -Wall -Werror -Wextra
+
+OBJS = $(SRC:.cpp=.o)
+
+all : $(NAME)
+
+.cpp.o:
+	$(CC) $(CFLAGS) -c $^ -o $@
+
+$(NAME) : $(OBJS)
+	$(CC) $(CFLAGS) $(SRC) -o $(NAME)
+
+clean:
+	rm -f $(OBJS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re : fclean all
